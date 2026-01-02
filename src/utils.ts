f29a525f830e2f69f6459a310dd6f3ff127c245c
unknown <vuluc5629@gmaol.com>
2026-01-02 12:04:51 +0700

    Version 0.7.0

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 // import * as os from 'os';
 import OpenAI from "openai";
 import { getSecret } from './secretManager';
+const config = vscode.workspace.getConfiguration("callOpenAI");
 
 export async function fetchWithTimer<T>(prompt: string, fn: (output: string) => Promise<T>, max_tokens? : number) {
     const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
@@ -17,16 +18,53 @@ export async function fetchWithTimer<T>(prompt: string, fn: (output: string) => 
         status.text = `$(sync~spin) Loading… ${seconds}s`;
     }, 1000);
 
-    const config = vscode.workspace.getConfiguration("callOpenAI");
-
-    const mode = config.get<"offline" | "online">("mode");
-    const url =
-        mode === "offline"
-            ? config.get<string>("offlineUrl")
-            : config.get<string>("onlineUrl");
-    logToFile(mode || '', url || '')
-
+    
     try {
+        const mode = config.get<"offline" | "online">("mode") || 'online';
+
+        // const apiKey = await getSecret();
+        // const client = new OpenAI({ apiKey });
+        const max_output_tokens = max_tokens || 1024
+
+        // const response = await client.responses.create({
+        //     model: max_output_tokens > 1024 ? 'gpt-4.1' : 'gpt-5.2',
+        //     input: prompt,
+        //     max_output_tokens,
+        // });
+        const model = mode == 'offline' ? 'local-llama' :  max_output_tokens > 1024 ? 'gpt-4.1' : 'gpt-5.2'
+        const response = await getResponse(model, prompt, max_tokens)
+        logToFile(model, prompt, response);
+        await fn(response);
+        // return result;
+    } catch (err: any) {
+        console.error('OpenAI call failed:', err);
+        vscode.window.showErrorMessage(`OpenAI error: ${err.message ?? String(err)}`);
+    } finally {
+        clearInterval(interval);
+        status.dispose();
+    }
+}
+
+async function getResponse(model: string, prompt: string, max_tokens? : number) {
+    if (model === 'local-llama') {
+        const url = config.get<string>("offlineUrl") || ''
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                model: "local-llama",
+                messages: [
+                    // { role: "system", content: "You are a coding assistant" },
+                    { role: "user", content: prompt }
+                ],
+                temperature: 0.2,
+            }),
+        });
+        const data = (await res.json()) as any
+        return data.choices?.[0]?.message?.content ?? '{}'
+    } else {
         const apiKey = await getSecret();
         const client = new OpenAI({ apiKey });
         const max_output_tokens = max_tokens || 1024
@@ -36,16 +74,7 @@ export async function fetchWithTimer<T>(prompt: string, fn: (output: string) => 
             input: prompt,
             max_output_tokens,
         });
-        logToFile(prompt, response.output_text ?? '{}');
-
-        await fn(response.output_text ?? '{}');
-        // return result;
-    } catch (err: any) {
-        console.error('OpenAI call failed:', err);
-        vscode.window.showErrorMessage(`OpenAI error: ${err.message ?? String(err)}`);
-    } finally {
-        clearInterval(interval);
-        status.dispose();
+        return response.output_text ?? '{}'
     }
 }
 
@@ -57,7 +86,7 @@ export async function fetchWithTimer<T>(prompt: string, fn: (output: string) => 
  * @param input - The input string to log (usually the user's input or request).
  * @param response - The response string to log (usually the application's output or response).
  */
-function logToFile(input: string, response: string) {
+function logToFile(model: string, input: string, response: string) {
     // const logDir = path.join('D:\backup\call-openai', 'logs');
     const logDir = String.raw`D:\backup\call-openai\logs`;
     if (!fs.existsSync(logDir)) {
@@ -68,6 +97,7 @@ function logToFile(input: string, response: string) {
     const logEntry = [
         `${'-'.repeat(60)}<${new Date().toISOString()}>${'-'.repeat(60)}`,
         `[${input.substring(0, 150)}]`,
+        `-----<${model}>----`,
         `${response}`,
         '\n'
     ].join('\n');
